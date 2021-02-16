49de795704afe80f98648911221e50431be010f1
sarhani saad <saad1996kenitra@gmail.com>
2021-02-16 13:47:48 +0100

    make it better2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook',function(req,res){
     console.log(req.body.entry[0].messaging)
     console.log(req.body.entry[0].messaging[0])
     if(req.body.entry[0].messaging[0].message.text)
-        sendText(sender,"Text hello"+req.body.entry[0].messaging[0].text)
+        sendText(req.body.entry[0].messaging[0].sender.id,"Text hello"+req.body.entry[0].messaging[0].message.text)
     // let msg=req.body.entry[0].messaging;
     //     console.log("hgfhgttttttttttfhg"+req.body.entry)
     //     let sender=msg[0].sender.id;
