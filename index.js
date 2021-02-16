1abbd7c0ff2865fe7a00c4fd3c4798bbec10f1e9
sarhani saad <saad1996kenitra@gmail.com>
2021-02-16 11:18:17 +0100

    make it better1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const { response } = require('express');
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
+token="EAANJgBYQmhcBAL0IsuyXQCWvRLpNkBTEZCfip6GuwjkZAW7tyctyTcQ6553ZC8TPezCZAVNOtxbNBRdZAZCuCj7uR87od9tfpdfYrZBZC9Qh4O7N9N4SloYi3Lp0WqI4ChZAvV0QxE1qjQAEjRBeqeIZBkhwlCp0A1FRWJzbAGgygLagZDZD"
 app.get('/',function(req,res){
     res.send('hello world')
 })
@@ -22,6 +23,38 @@ app.get('/webhook',function(req,res){
     }
     res.send('token non valid');
 })
+
+app.post('/webhook',function(req,res){
+    let msg=req.body.entry[0].messaging_events;
+    for(int i=0;i<msg.length;i++){
+        let event = msg[i];
+        let sender=event.sender.id;
+        if(event.message && event.message.text){
+            let text = event.message.text;
+            sendText(sender,"Text "+ text.substring(0,100))
+        }
+    }
+    res.sendStatus(200);
+})
+
+function sendText(sender,text){
+    let msgData = {text:text}
+    request({
+        url:"https://graph.facebook.com/me/messages",
+        qs:{access_token , token},
+        method : "POST",
+        json:{
+            receipt:{id:sender},
+            message:msgData
+        }
+    },function(error,esponse,body){
+        if(error){
+            console.log("sending error")
+        }else if(response.body.error){
+            console.log("error body")
+        }
+    })
+}
 
 app.listen(app.get('port'),function(){
     console.log('excution port');
