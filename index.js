7aad84e5844888a2e98e8f8ca0cfbd6b6dd37e65
sarhani saad <saad1996kenitra@gmail.com>
2021-02-16 12:18:04 +0100

    make it better2

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ app.post('/webhook',function(req,res){
     for(let i=0;i<msg.length;i++){
         let event = msg[i];
         let sender=event.sender.id;
-        if(event.message && event.message.text){
-            let text = event.message.text;
-            sendText(sender,"Text "+ text.substring(0,100))
-        }
+        sendText(sender,"Text hello")
     }
     console.log("test  +=++"+req.body.entry[0].messaging[0].sender.id)
     res.sendStatus(200);
