3c18e115ff57c6c73076ff514bf5d90736dbb8d1
sarhani saad <saad1996kenitra@gmail.com>
2021-02-16 11:47:47 +0100

    make it better3

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post('/webhook',function(req,res){
     //     }
     // }
     console.log(req.body.entry[0].messaging)
-    sendText(msg[0].event.sender.id,"salut ca va,comment va tu ")
+    //sendText(msg[0].event.sender.id,"salut ca va,comment va tu ")
     res.sendStatus(200);
 })
 
