1e4a1a1d508252b9887d1864392c25cc18246b49
sarhani saad <saad1996kenitra@gmail.com>
2021-02-16 13:56:44 +0100

    make it better2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/webhook',function(req,res){
     console.log(req.body.entry[0].messaging)
     console.log(req.body.entry[0].messaging[0])
     if(req.body.entry[0].messaging[0].message['text'])
-    if(req.body.entry[0].messaging[0].message['text'] == 'Comment vas-tu ?')
+    if(req.body.entry[0].messaging[0].message['text'].localeCompare('Comment vas-tu ?'))
         sendText(req.body.entry[0].messaging[0].sender.id,"Très bien et vous ?")
     // let msg=req.body.entry[0].messaging;
     //     console.log("hgfhgttttttttttfhg"+req.body.entry)
