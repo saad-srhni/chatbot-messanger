2d26956d9a6de3d8944c33251d4a223719be122b
sarhani saad <saad1996kenitra@gmail.com>
2021-02-17 13:24:02 +0100

    update

--- index.js
+++ index.js
@@ -27,48 +27,57 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {
     console.log(req.body.entry[0].messaging)
     console.log(req.body.entry[0].messaging[0]['attachments'])
+    let idsender = req.body.entry[0].messaging[0].sender.id;
+    let msgData;
     if (req.body.entry[0].messaging[0].message['text']) {
         if (req.body.entry[0].messaging[0].message['text'].localeCompare('Comment vas-tu ?') == 0) {
-            sendText(req.body.entry[0].messaging[0].sender.id, "Très bien et vous ?")
-
-
+            msgData = {
+                text: "Très bien et vous ?",
+                quick_replies: [
+                    {
+                        content_type: "text",
+                        title: "Je vais bien,merci",
+                        payload: "PYLOAD_ONE"
+                    },
+                    {
+                        content_type: "text",
+                        title: "Non, ça ne va pas",
+                        payload: "PYLOAD_Two"
+                    }
+                ]
+            }
+            sendText(idsender, msgData)
         }
-        else sendText(req.body.entry[0].messaging[0].sender.id, req.body.entry[0].messaging[0].message['text'])
+        else {
+            msgData = {
+                text: req.body.entry[0].messaging[0].message['text']
+            }
+            sendText(idsender, msgData)
+        }
+    } else if (req.body.entry[0].messaging[0]['attachments']) {
+        msgData = {
+            text: "Je ne sais pas traiter ce type de demande"
+        }
+        sendText(idsender, msgData)
     }
-    // let msg=req.body.entry[0].messaging;
-    //     console.log("hgfhgttttttttttfhg"+req.body.entry)
-    //     let sender=msg[0].sender.id;
-    //     console.log("====))m="+req.body.entry[0])
-    //     if(req.body.entry[0].messaging[0] && req.body.entry[0].messaging[0].text)
-    //     sendText(sender,"Text hello"+req.body.entry[0].messaging[0].text)
-    // console.log("test  +=++"+req.body.entry[0].messaging[0].sender.id)
     res.sendStatus(200);
 })
 
-function sendText(sender, text) {
-    let msgData = { text: text }
+function sendText(sender, msgData) {
     request({
         url: "https://graph.facebook.com/v9.0/me/messages",
         qs: { access_token: token },
         method: "POST",
         json: {
             recipient: { id: sender },
-            message: {
-                ...msgData,
-                quick_replies: [
-                    {
-                        content_type: "text",
-                        title: "salam cv 3lik",
-                        payload: "PYLOAD_ONE"
-                    }
-                ]
-            },
-        }
+            message: msgData
+        },
+    }
     }, function (error, response, body) {
-        if (error) {
-            console.log("sending error" + error)
-        }
-    })
+    if (error) {
+        console.log("sending error" + error)
+    }
+})
 }
 
 app.listen(app.get('port'), function () {
