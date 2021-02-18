8135ff10ea41c769a2a64c440440d4f4812a22b1
sarhani saad <saad1996kenitra@gmail.com>
2021-02-18 11:18:54 +0100

    update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
     console.log(req.body.entry[0].messaging)
-    // console.log(req.body.entry[0].messaging[0].message['attachments'])
+    console.log(req.body.entry[0].messaging[0].message['attachments'])
     let idsender = req.body.entry[0].messaging[0].sender.id;
     let msg = req.body.entry[0].messaging[0];
     if (msg['message']) {
